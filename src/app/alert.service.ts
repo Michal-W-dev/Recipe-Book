7b271fcbba46c8cc70d4c6bd46da11ac18e7be63
Michal-W-dev <maikey20@gmail.com>
2022-04-19 17:44:24 +0200

    Add alert component & service.

---
+++ src/app/alert.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AlertService {
+  alertEmitter = new Subject<{ show: boolean, msg: string, cssBorder: string }>()
+  constructor() { }
+
+  showAlert(msg = '', borderColor?: 'danger' | 'success') {
+    const isBorder = (color: 'danger' | 'success') => borderColor === color
+    const cssBorder = `2px solid ${isBorder('success') ? 'lime' : isBorder('danger') ? 'deeppink' : 'white'}`
+
+    this.alertEmitter.next({ show: true, msg, cssBorder });
+    setTimeout(() => this.alertEmitter.next({ show: false, msg: '', cssBorder }), 3000)
+  }
+
+}
---
+++ src/app/alert/alert.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AlertService } from '../alert.service';
+
+@Component({
+  selector: 'app-alert',
+  templateUrl: './alert.component.html',
+  styleUrls: ['./alert.component.scss']
+})
+export class AlertComponent implements OnInit {
+  show = false;
+  message = '';
+  border = ''
+  alertSub: Subscription;
+
+  constructor(private alertService: AlertService) { }
+
+  ngOnInit(): void {
+    this.alertSub = this.alertService.alertEmitter.subscribe(({ show, msg, cssBorder }) => {
+      this.show = show;
+      this.message = msg;
+      this.border = cssBorder
+    })
+  }
+
+}
